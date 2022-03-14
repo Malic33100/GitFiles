f6695ccfe52e6174f4c47b006d28888d3881f05a
Malik <mertus.malik@gmail.com>
2022-03-14 18:30:23 -0400

    table and chair

---
+++ TablesAndChairs.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class TablesAndChairs 
+{
+public static void main (String[] args){
+    JOptionPane.showMessageDialog(null,"x                   x"); 
+    JOptionPane.showMessageDialog(null,"x                   x");
+    JOptionPane.showMessageDialog(null,"x                   x");
+    JOptionPane.showMessageDialog(null,"xxxx  xxxxxxxxx  xxxx");
+    JOptionPane.showMessageDialog(null,"x  x  x       x  x  x");
+    JOptionPane.showMessageDialog(null,"x  x  x       x  x  x");
+};
+    }
+
